fdf7fb7c116aae376b99dcaad72f725f762c28c0
Jake Velasco <jakevelasco40@gmail.com>
2021-12-01 12:08:28 +0800

    [remove] node-fetch

--- routes/face-api/monkeyPatch.js
+++ routes/face-api/monkeyPatch.js
@@ -1,10 +1,8 @@
 const canvas = require('canvas')
 const faceapi = require('@vladmandic/face-api')
-const fetch = require('node-fetch')
 module.exports = {
     monkeyPatchFaceApiEnv: () => {
         const { Canvas, Image, ImageData } = canvas
         faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
-        faceapi.env.monkeyPatch({ fetch: fetch })
     }
 }
